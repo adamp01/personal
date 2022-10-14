981e7634e940c49a13b55000bee3db04bc19d453
Adam Wood <adamwoodce@gmail.com>
2022-10-14 17:32:24 +0100

    feat: use layout property in Figure

--- components/blog/Figure.tsx
+++ components/blog/Figure.tsx
@@ -5,15 +5,22 @@ import styles from '../../styles/Home.module.css';
 type FigureProps = {
   caption: string;
   src: string;
-  width: string;
-  height: string;
+  width?: string;
+  height?: string;
+  layout: 'fixed' | 'fill' | 'intrinsic' | 'responsive' | undefined;
 };
 
-export default function Figure({ caption, src, width, height }: FigureProps) {
+export default function Figure({
+  caption,
+  src,
+  width,
+  height,
+  layout
+}: FigureProps) {
   return (
     <div className={styles.figure}>
-      <Image src={src} width={width} height={height} />
-      <br />
+      <Image src={src} height={height} width={width} layout={layout} />
+      {layout === 'responsive' ? <div /> : <br />}
       <p className={styles.caption}>{caption}</p>
     </div>
   );

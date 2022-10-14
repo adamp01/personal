8b125bd2279e9efecf75e60d2827f2e10da124ae
Adam Wood <adamwoodce@gmail.com>
2022-10-14 18:14:15 +0100

    style: neaten adjacent post cards

--- components/blog/AdjacentPosts.tsx
+++ components/blog/AdjacentPosts.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link';
 
-import { ScrollingLink } from '../scrolling/ScrollingText';
 import styles from '../../styles/Home.module.css';
 
 import type { AdjacentPostBinding, AdjacentPosts } from '../../types/blog';
@@ -13,11 +12,7 @@ function AdjacentPost({ data, previous, next }: AdjacentPostBinding) {
       <p>{`${previous ? 'Previous' : 'Next'}`}</p>
       <h3>
         <Link href={`/blog/${data?.slug}`} passHref>
-          {data?.data.title.length > 30 ? (
-            <ScrollingLink>{data?.data.title}</ScrollingLink>
-          ) : (
-            data?.data.title
-          )}
+          {data?.data.title}
         </Link>
       </h3>
     </div>
@@ -26,7 +21,7 @@ function AdjacentPost({ data, previous, next }: AdjacentPostBinding) {
 
 export default function AdjacentPosts({ previous, next }: AdjacentPosts) {
   return (
-    <div>
+    <div className={styles['adjacent-post-container']}>
       {previous ? <AdjacentPost data={previous} previous={'true'} /> : <div />}
       {next ? <AdjacentPost data={next} next={'true'} /> : <div />}
     </div>

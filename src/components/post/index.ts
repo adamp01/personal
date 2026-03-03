6311f9acffe691738e6b530307a620d95c56f61f
Adam Wood <adamwoodce@gmail.com>
2026-03-03 17:02:51 +0000

    refactor: split out trip sidebar components

--- src/components/post/index.ts
+++ src/components/post/index.ts
@@ -1,3 +1,7 @@
 export { default as ImageBase } from "./ImageBase.astro";
 export { default as HeroImage } from "./HeroImage.astro";
 export { default as TripSidebar } from "./TripSidebar.astro";
+export { default as SidebarBox } from "./SidebarBox.astro";
+export { default as TripOverview } from "./TripOverview.astro";
+export { default as RouteMap } from "./RouteMap.astro";
+export { default as PlacesVisited } from "./PlacesVisited.astro";

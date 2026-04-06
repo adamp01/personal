4d50528ed5b3853202987e238c681888231c6299
Adam Wood <adamwoodce@gmail.com>
2026-04-06 18:24:26 +0100

    style: code blocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,10 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), react()],
+  markdown: {
+    shikiConfig: {
+      theme: "one-dark-pro",
+      wrap: false,
+    },
+  },
 });
